f6dd080087fe11e88ead7abdb2ac53163a63b1f5
Sergey Skoptsov <flicus@gmail.com>
2022-11-04 18:45:41 +0200

    back to declarative http client

--- src/main/java/org/schors/gos/micro/tg/TgClient.java
+++ src/main/java/org/schors/gos/micro/tg/TgClient.java
@@ -1,12 +1,9 @@
 package org.schors.gos.micro.tg;
 
-import io.micronaut.context.annotation.Value;
-import io.micronaut.core.type.Argument;
-import io.micronaut.core.type.GenericArgument;
-import io.micronaut.http.client.HttpClient;
+import io.micronaut.core.async.annotation.SingleResult;
+import io.micronaut.http.annotation.Body;
+import io.micronaut.http.annotation.Post;
 import io.micronaut.http.client.annotation.Client;
-import jakarta.inject.Inject;
-import jakarta.inject.Singleton;
 
 import org.telegram.telegrambots.meta.api.methods.BotApiMethod;
 import org.telegram.telegrambots.meta.api.methods.updates.GetUpdates;
@@ -15,29 +12,16 @@ import org.telegram.telegrambots.meta.api.objects.Message;
 import org.telegram.telegrambots.meta.api.objects.Update;
 import reactor.core.publisher.Mono;
 
-import java.lang.reflect.ParameterizedType;
 import java.util.ArrayList;
 
-import static io.micronaut.http.HttpRequest.POST;
+@Client("telegram")
+public interface TgClient {
 
-@Singleton
-public class TgClient {
+  @SingleResult
+  @Post("getupdates")
+  Mono<ApiResponse<ArrayList<Update>>> getUpdates(@Body GetUpdates getUpdates);
 
-  @Client("telegram")
-  @Inject
-  HttpClient client;
-
-  @Value("${gosb.bot.token}")
-  String token;
-
-  Mono<ApiResponse<ArrayList<Update>>> getUpdates(GetUpdates getUpdates) {
-    return Mono.from(client
-    .retrieve(POST(token + "/getupdates", getUpdates), new GenericArgument<ApiResponse<ArrayList<Update>>>() {}));
-  }
-
-
-  Mono<ApiResponse<Message>> sendMessage(BotApiMethod sendMessage) {
-    return Mono.from(client
-    .retrieve(POST(token + "/sendmessage", sendMessage), new GenericArgument<ApiResponse<Message>>(){}));
-  }
+  @SingleResult
+  @Post("sendmessage")
+  Mono<ApiResponse<Message>> sendMessage(@Body BotApiMethod sendMessage);
 }
